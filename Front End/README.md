89c93563974e0ed42913183127e94f1d833f8b31
unknown <zamano.jacob@gmail.com>
2019-09-08 14:43:16 -0700

    adding The-ushe 1.0 files

---
+++ Front End/README.md
@@ -0,0 +1,15 @@
+# The Ushe \[t͟hə ˈyüzh]
+
+The one-stop app for all your meal trains, office coffee runs, and "can I get you something on the way?" requests, and getting them all right, every time.
+
+Save your favorites, share, and discover.
+
+With The Ushe, you don't have to be a regular to get the usual.
+
+------------------------------------
+
+### Technologies
+
+### Dependencies
+ - graphql
+ - apollo
---
+++ Front End/build/precache-manifest.068507e9dda7f01cbc94de0becd2f0ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3537a63f9134c6d27559dc90c7bbdf77",
+    "url": "/cfpaige/the-ushe/index.html"
+  },
+  {
+    "revision": "f1350404dd643c2b7585",
+    "url": "/cfpaige/the-ushe/static/css/main.d5d9c3bb.chunk.css"
+  },
+  {
+    "revision": "d63a9712a2f9ebddd2c8",
+    "url": "/cfpaige/the-ushe/static/js/2.29080e66.chunk.js"
+  },
+  {
+    "revision": "f1350404dd643c2b7585",
+    "url": "/cfpaige/the-ushe/static/js/main.5291ed8f.chunk.js"
+  },
+  {
+    "revision": "96e5f264327243251109",
+    "url": "/cfpaige/the-ushe/static/js/runtime~main.c22481a4.js"
+  }
+]);
