81baac37df26592893b0da50c77f8b77739e7122
unknown <zamano.jacob@gmail.com>
2019-09-09 19:40:37 -0700

    front end folder turned into stock create-react-app file

--- Front End/src/index.js
+++ Front End/src/index.js
@@ -1,63 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect
-} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import Home from "./pages/Home/Home";
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Profile from './pages/Profile/Profile';
-import Feed from './pages/Feed/feed';
-import withSession from './components/withSession/withSession'
+ReactDOM.render(<App />, document.getElementById('root'));
 
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import profileSearch from './pages/Profile/profileSearch';
-
-const client = new ApolloClient({
-    uri: "https://the-ushe-server.herokuapp.com/graphql",
-    fetchOptions: {
-        credentials: 'include'
-    },
-    request: operation => {
-        const token = localStorage.getItem('token')
-        operation.setContext({
-            headers: {
-                authorization: token
-            }
-        })
-    },
-    onError: ({ networkError}) => {
-        if(networkError){
-            console.log('Network Error', networkError)
-        }
-    }
-})
-
-const Root = ({refetch, session}) => (
-    <Router>
-    <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" render={()=> <Login refetch={refetch}/>} />
-    <Route exact path="/register" render={()=> <Register refetch={refetch}/>} />
-    <Route exact path="/profile" render={()=> <Profile session={session}/>}/>
-    <Route path="/profiles/:username" component={profileSearch}/>
-    <Route exact path="/feed" render={()=> <Feed session={session}/>}/>
-    <Redirect to="/" />
-    </Switch>
-    </Router>
-)
-
-const RootWithSession = withSession(Root);
-
-ReactDOM.render(
-    <ApolloProvider client={client}>
-      <RootWithSession />
-    </ApolloProvider>,
-    document.getElementById("root")
-  );
-  
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
