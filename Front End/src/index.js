1ec84363410dfc144c6962ed7ed4f12d136e974c
unknown <zamano.jacob@gmail.com>
2019-09-09 20:10:24 -0700

    files

--- Front End/src/index.js
+++ Front End/src/index.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from 'react-router-dom';
+import Home from "./pages/Home/Home";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const Root = () => (
+    <Router>
+    <Route exact path="/" component={Home} />
+    </Router>
+)
+
+
+ReactDOM.render(<Root/>, document.getElementById('root'));
