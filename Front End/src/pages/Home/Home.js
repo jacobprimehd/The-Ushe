3442b50ae3c1e4d1a11caa19edd0dae850740150
unknown <zamano.jacob@gmail.com>
2019-09-10 17:51:49 -0700

    added files

--- Front End/src/pages/Home/Home.js
+++ Front End/src/pages/Home/Home.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import "./Home.css";
 
 
+
 class Home extends Component {
+  componentDidMount() {
+    document.body.classList.add("homebg");
+  }
   render() {
     return (
       <div>
       <h1 className= "header-main" >The Ushe</h1>
       <h2 className= "header-home"> What's your Ushe?</h2>
       <h3 className= "header-second" > Share your favorite food and drink orders with the world. </h3>
-      <button type="button" className="btn btn-outline-primary"><a href="/login">Login</a></button>
-      <button type="button" className="btn btn-outline-primary"><a href="/register">Register</a></button>
+      <a href="/login"><button type="button" className="btn btn-outline-primary">Login</button></a>
+      <a href="/register"><button type="button" className="btn btn-outline-primary">Register</button></a>
       </div>
     );
   }
